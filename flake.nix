689414d1697e5787d643738c7aaeb540bb80579e
Cormac Cannon <cormacc@gmail.com>
2024-09-19 13:17:35 +0100

    new(nix): Added flake-based devshell setup
    Just clone the repo and then 'direnv allow'

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix-flake-based development environment for application.garden";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    garden.url = "github:nextjournal/garden-cli";
+  };
+
+  outputs = { self, nixpkgs, garden }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            garden.overlays.default
+          ];
+        };
+      });
+    in {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+            buildInputs = [ pkgs.garden ];
+          };
+      });
+    };
+}
